3b049d4a7bf71db8edc641c2dcd20a8e92c7c815
jumanazar <jsr1611@outlook.com>
2024-08-18 16:14:59 +0900

    updated some validation and error msg hadling for adding new word to database

--- src/app/components/add-word/add-word.component.ts
+++ src/app/components/add-word/add-word.component.ts
@@ -3,6 +3,7 @@ import { catchError, of } from 'rxjs';
 import { Category, Word } from 'src/app/models/word';
 import { CategoryMapping } from 'src/app/mappings/category-mapping';
 import { DictionaryService } from 'src/app/services/DictionaryService';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-word',
@@ -20,8 +21,11 @@ export class AddWordComponent {
 
   categories: Category[] = Object.values(Category); // Enum values
   categoryLabels = CategoryMapping; // Uzbek labels
+  successMsg = "";
 
-  constructor(private dictionaryService: DictionaryService) {}
+  constructor(
+    private dictionaryService: DictionaryService,
+  ) {}
 
   addWord(): void {
     // Validate required fields
@@ -32,13 +36,21 @@ export class AddWordComponent {
 
     this.dictionaryService.createDictEntry(this.newWord).pipe(
       catchError((error) => {
-        console.error('Error creating word entry:', error);
+        console.error('Error creating word entry:', error.error);
+        this.successMsg = "So'zni saqlashda muammoga duch kelindi.";
+        let errMsg = error.error ? error.error.message : error.message;
+        alert(errMsg);
         return of(null);
       })
     ).subscribe((data) => {
       if (data) {
         console.log('Server response:', data);
         this.resetForm();
+        this.successMsg = "So'z muvaffaqiyatli saqlandi...";
+        setTimeout(() => {
+          window.location.reload();  
+        }, 2000)
+
       }
     });
   }
