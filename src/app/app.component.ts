7373ea4bf0fff876f3284b21cab706f1c5d68950
Jumanazar <jsr1611@outloo9k.com>
2024-08-21 21:26:30 +0900

    - Updated visit count logic to wait for recording to finish for 1 second to include the visitor itself in the updated count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from './services/UserService';
 import { Visit } from './models/visit';
 
@@ -7,36 +7,36 @@ import { Visit } from './models/visit';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Arabic Course';
   dailyVisitData!: Visit;
   dailyVisitCount: number = 0;
-  
+
   constructor(
-    private userService: UserService,
-    private cdRef: ChangeDetectorRef
-  ) {}
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
     let localCount = localStorage.getItem("uniqueVisitors");
-    if(localCount && Number(localCount) > this.dailyVisitCount){
+    if (localCount && Number(localCount) > this.dailyVisitCount) {
       this.dailyVisitCount = Number(localCount);
     }
     this.userService.recordVisit().subscribe({
-      next: response => console.log('Tashrif muvaffaqiyatli qayd etildi. Tashrifingiz uchun rahmat!'),
+      next: () => console.log('Tashrif muvaffaqiyatli qayd etildi. Tashrifingiz uchun rahmat!'),
       error: error => console.error('Tashrif qayd etishda xatolik bo`ldi', error)
     });
 
-    this.userService.getVisitsCount().subscribe({
-      next: (visitData: Visit) => {
-        this.dailyVisitData = visitData;
-        if(visitData.uniqueVisitors > this.dailyVisitCount){
-          this.dailyVisitCount = visitData.uniqueVisitors;
-          this.cdRef.detectChanges();
-        }
-        localStorage.setItem("uniqueVisitors", `${this.dailyVisitCount}`)
-      },
-      error: (error: any) => console.error('Tashriflar ma`lumotini yuklashda xatolik bo`ldi', error)
-    });
+    setTimeout(() => {
+      this.userService.getVisitsCount().subscribe({
+        next: (visitData: Visit) => {
+          this.dailyVisitData = visitData;
+          if (visitData.uniqueVisitors > this.dailyVisitCount) {
+            this.dailyVisitCount = visitData.uniqueVisitors;
+          }
+          localStorage.setItem("uniqueVisitors", `${this.dailyVisitCount}`)
+        },
+        error: (error: any) => console.error('Tashriflar ma`lumotini yuklashda xatolik bo`ldi', error)
+      });
+    }, 1000);
   }
 }
